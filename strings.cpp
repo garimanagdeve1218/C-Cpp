800f0b17e9103d16d0a2fe8cb6cc66fefdd60a27
GARIMA NAGDEVE <144271162+garimanagdeve1218@users.noreply.github.com>
2024-04-19 03:24:28 +0530

    Add files via upload

---
+++ strings.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+void SayHi(string name,int age){
+	cout<<"HI! "<<name<<" you are "<<age;
+}
+
+int main(){
+	SayHi("Garima Nagdeve",20);
+	
+	return 0;
+}
---
+++ variables.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+	string DataType1="Garima Nagdeve";
+	int DataType2= 9873479;
+	char DataType3='@';
+	float DataType4=2.568967888f;
+	double DataType5=3.6784575655;
+	bool isMale=true;
+	
+	cout<<DataType1<<endl;
+	cout<<DataType2<<endl;
+	cout<<DataType3<<endl;
+	cout<<DataType4<<endl;
+	cout<<DataType5<<endl;
+	cout<<isMale<<endl;
+
+	
+	
+	return 0;
+}
