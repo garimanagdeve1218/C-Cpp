504bd442e9e8b8431e92eb240bced10b64962ace
GARIMA NAGDEVE <144271162+garimanagdeve1218@users.noreply.github.com>
2024-04-19 19:22:34 +0530

    Add files via upload

---
+++ size of datatypes.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+//Program to Find Size of int, float, double and char in Your System
+int main(){
+	cout<<"Size of char is: "<<sizeof(char)<<" bytes."<<endl;
+	cout<<"Size of int is: "<<sizeof(int)<<" bytes."<<endl;
+	cout<<"Size of double is: "<<sizeof(double)<<" bytes."<<endl;
+	cout<<"Size of float is: "<<sizeof(float)<<" bytes."<<endl;
+	cout<<"Size of bool is: "<<sizeof(bool)<<" bytes."<<endl;
+	cout<<"Size of string is: "<<sizeof(string)<<" bytes."<<endl;
+	
+	return 0;
+}
