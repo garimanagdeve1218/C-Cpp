b429d1782bcbad44140a4fa1dce903cb8b6a952e
GARIMA NAGDEVE <144271162+garimanagdeve1218@users.noreply.github.com>
2024-04-22 20:32:17 +0530

    Add files via upload

---
+++ hello world.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+	
+	int number;
+		cout<<"Enter your favourite number: ";
+		cin>>number;
+		cout<<"Your favourite number is: "<<number<<endl;
+	
+	return 0;
+	
+}
