3cd9450fecf67326150cc825225546a4c93da895
GARIMA NAGDEVE <144271162+garimanagdeve1218@users.noreply.github.com>
2024-04-24 21:48:15 +0530

    Add files via upload

---
+++ if statements part2.cpp
@@ -0,0 +1,17 @@
+
+#include<iostream>
+using namespace std;
+int main(){
+	
+	int age=20;
+	string name="Garima";
+	bool isFemale="true";
+	
+	
+	
+	cout<<&age<< endl;
+	cout<<&name<< endl;
+	cout<<&isFemale<< endl;
+	
+	return 0;
+}	
