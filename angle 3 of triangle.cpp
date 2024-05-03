67a3d42282ba1e7be9c953e6438bff722c188a54
GARIMA NAGDEVE <144271162+garimanagdeve1218@users.noreply.github.com>
2024-05-04 00:57:40 +0530

    Add files via upload

---
+++ angle 3 of triangle.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+	float ang1, ang2, ang3;
+	
+	cout<<"what is angle 1 ?";
+	cin>>ang1;
+	cout<<"what is angle 2 ?";
+	cin>>ang2;
+	
+		
+	
+	ang3= 180 - ( ang1 + ang2 );
+	cout<<"the value of angle 3 is : "<<ang3<<endl;
+	
+	return 0;
+}
